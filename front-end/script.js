2b1dfeb3731ca68537385a76140a28622ad75413
Reloaded <deepak@designandcode.tech>
2024-07-09 23:14:55 +0530

    layout change

--- front-end/script.js
+++ front-end/script.js
@@ -1,8 +1,8 @@
 const slider = document.querySelector('.slider');
-const person = document.querySelector('.person-slider');
+// const person = document.querySelector('.person-slider');
 const slides = slider.querySelectorAll('.slide');
 const dotsContainer = slider.querySelector('.dots');
-const image = person.querySelector('.person-slides');
+// const image = person.querySelector('.person-slides');
 let currentIndex = 0;
 
 function showSlide(index) {
@@ -32,24 +32,15 @@ function updateActiveDot() {
   });
 }
 
-function updateImage(){
-  const people = image.querySelectorAll('.person-slide');
-  people.forEach((i, index) => {
-    i.classList.toggle('active', index === currentIndex);
-  });
-}
-
 function autoSlide() {
   currentIndex = (currentIndex + 1) % slides.length;
   showSlide(currentIndex);
   updateActiveDot();
-  updateImage()
 }
 
 
 createDots();
 showSlide(currentIndex);
 updateActiveDot();
-updateImage()
 setTimeout(autoSlide, 5000); // Display each slide for 10 seconds
 setInterval(autoSlide, 5000); // Auto slide every 8 seconds
--- sterling-reviews.php
+++ sterling-reviews.php
@@ -9,7 +9,7 @@
 
  add_action( 'wp_enqueue_scripts', function () {
   wp_enqueue_style( 'testimonials', plugins_url( '/front-end/style.css' , __FILE__ ), false, '1.0', 'all');
-  wp_enqueue_style( 'font-awesome-6.4', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',false,'6.4','all');
+  // wp_enqueue_style( 'font-awesome-6.4', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',false,'6.4','all');
   if(is_front_page()) {
   wp_enqueue_script('testimonials',plugins_url( '/front-end/script.js', __FILE__ ), false,'1.0', true );}
 } );
@@ -18,28 +18,27 @@ add_shortcode( 'google_reviews_sterling', function()
 {
   $str = file_get_contents(plugins_url( '/reviews.json', __FILE__ ));
   $json = json_decode($str, true); 
+  $data = $json['reviews'];
 
-  $image = ""; $slides = "";$count = 1;$slide_no = 0;
-  foreach ($json['reviews'] as $key => $value) {
-    if($count===6) break;
-    if($value['starRating']!=='FIVE') continue;
- 
-    $image .= '<div class="person-slide slide-'.$slide_no.'">
-    <img src="https://dummyimage.com/200x200/000/fff.jpg" alt="'.$value["reviewer"]["displayName"].'">
-  </div>';
+  $image = ""; $slides = ""; $count = 0;
+
+  for ($i=0; $count < 10 ; $i++) { 
+      $random = (int)rand(0, sizeof($data));
+      
+      if($data[$random]['starRating']!=="FIVE" || !array_key_exists("comment",$data[$random])) continue;
   
   $slides .= '<div class="slide">
   <p class="description elementor-heading-title">
-    '.$value["comment"].'
+    '.$data[$random]["comment"].'
   </p>
   <div class="name">
-  '.$value["reviewer"]["displayName"].'
+  '.$data[$random]["reviewer"]["displayName"].'
   </div>
   <div class="ratings">
   <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
   </div>
 </div>'; 
-  $count++;$slide_no++;
+$count ++;
 }
     return '
     <div class="testimonials">
@@ -47,7 +46,21 @@ add_shortcode( 'google_reviews_sterling', function()
       <div class="col-40">
         <div class="person-slider">
           <div class="person-slides">
-          '.$image.'
+          <div class="person-slide slide-0">
+    <img src="https://dummyimage.com/200x200/000/fff.jpg" alt="Sterling Google Reviews">
+  </div>
+          <div class="person-slide slide-1">
+    <img src="https://dummyimage.com/200x200/000/fff.jpg" alt="Sterling Google Reviews">
+  </div>
+          <div class="person-slide slide-2">
+    <img src="https://dummyimage.com/200x200/000/fff.jpg" alt="Sterling Google Reviews">
+  </div>
+          <div class="person-slide slide-3">
+    <img src="https://dummyimage.com/200x200/000/fff.jpg" alt="Sterling Google Reviews">
+  </div>
+          <div class="person-slide slide-4">
+    <img src="https://dummyimage.com/200x200/000/fff.jpg" alt="Sterling Google Reviews">
+  </div>
           </div>
         </div>
       </div>
