ac5b2e7361c2b24b183efad71b95a376fe17d991
Reloaded <deepak@designandcode.tech>
2024-08-09 22:33:03 +0530

    ver 2.0

--- front-end/script.js
+++ front-end/script.js
@@ -1,46 +1,154 @@
-const slider = document.querySelector('.slider');
-// const person = document.querySelector('.person-slider');
-const slides = slider.querySelectorAll('.slide');
-const dotsContainer = slider.querySelector('.dots');
-// const image = person.querySelector('.person-slides');
-let currentIndex = 0;
+gsap.registerPlugin(MotionPathPlugin);
+const circlePath = MotionPathPlugin.convertToPath("#holder", false)[0];
+circlePath.id = "circlePath";
+document.querySelector("#viewBox").prepend(circlePath);
 
-function showSlide(index) {
-  const slideWidth = slides[0].offsetWidth;
-  const offset = -index * slideWidth;
-  slider.querySelector('.slides').style.transform = `translateX(${offset-5}px)`;
-}
+const slider = document.getElementById('g-reviews');
+const slides = slider.querySelectorAll('.content-wrapper .testimonial');
+const dotsContainer = slider.querySelector('.wrapper .dots');
 
-function createDots() {
-  slides.forEach((slide, index) => {
-    const dot = document.createElement('span');
-    dot.classList.add('dot');
-    dot.addEventListener('click', () => {
-      currentIndex = index;
-      showSlide(currentIndex);
-      updateActiveDot();
-      updateImage();
+createDot();
+
+let items = gsap.utils.toArray(".g-reviews .item"),
+    numItems = items.length,
+    itemStep = 1 / numItems,
+    wrapProgress = gsap.utils.wrap(0, 1),
+    snap = gsap.utils.snap(itemStep),
+    wrapTracker = gsap.utils.wrap(0, numItems),
+    tracker = { item: 0 };
+
+gsap.set(items, {
+    motionPath: {
+        path: circlePath,
+        align: circlePath,
+        alignOrigin: [0.5, 0.5],
+        end: i => i / items.length
+    }, scale: 0.9
+});
+
+const tl = gsap.timeline({ paused: true, reversed: true });
+
+tl.to('.g-reviews .wrapper', {
+    rotation: 360,
+    transformOrigin: 'center',
+    duration: 1,
+    ease: 'none'
+});
+
+tl.to(items, {
+    rotation: "-=360",
+    transformOrigin: 'center',
+    duration: 1,
+    ease: 'none',
+}, 0);
+
+tl.to(tracker, {
+    item: numItems,
+    duration: 1,
+    ease: 'none',
+    modifiers: {
+        item(value) {
+            return wrapTracker(numItems - Math.round(value))
+        }
+    }
+}, 0);
+
+items.forEach(function (el, i) {
+
+    el.addEventListener("click", function () {
+        let current = tracker.item,
+            activeItem = i;
+
+        if (i === current) {
+            return;
+        }
+
+        //set active item to the item that was clicked and remove active class from all items
+        document.querySelector('.g-reviews .item.active').classList.remove('active');
+        items[activeItem].classList.add('active');
+
+        let diff = current - i;
+
+        if (Math.abs(diff) < numItems / 2) {
+            moveWheel(diff * itemStep);
+        } else {
+            let amt = numItems - Math.abs(diff);
+
+            if (current > i) {
+                moveWheel(amt * -itemStep);
+            } else {
+                moveWheel(amt * itemStep);
+            }
+        }
     });
-    dotsContainer.appendChild(dot);
-  });
+});
+
+// start of document load
+document.addEventListener("DOMContentLoaded", function () {
+
+    slideShow();
+    moveWheel(-itemStep);
+    updateNumber()
+    
+    setInterval(function(){
+        moveWheel(-itemStep);
+        slideShow();
+        updateNumber()
+    }, 3000);
+
+    document.getElementById('next').addEventListener("click", function () {
+         moveWheel(-itemStep);
+         slideShow();
+         updateNumber()
+    });
+
+    document.getElementById('prev').addEventListener("click", function () {
+         moveWheel(itemStep);
+         slideShow();
+         updateNumber();
+    });
+
+});
+// end of document load
+
+
+function createDot() {
+    slides.forEach((item, i) => {
+        const dot = document.createElement('div');
+        dot.classList.add('item');
+        i === 0 ? dot.classList.add('active') : '';
+        dotsContainer.appendChild(dot);
+    });
 }
 
-function updateActiveDot() {
-  const dots = dotsContainer.querySelectorAll('.dot');
-  dots.forEach((dot, index) => {
-    dot.classList.toggle('active', index === currentIndex);
-  });
+//Actually define the slideShow()
+function slideShow() {
+    $=jQuery.noConflict();
+    let current = $('#content .active');
+    let next = current.next().length ? current.next() :  current.siblings().first();
+
+    current.hide().removeClass('active');
+    next.fadeIn("slow").addClass('active');
+};
+
+function updateNumber(){
+    document.getElementById("number").innerText = `${tracker.item+1}/${numItems}`
 }
 
-function autoSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
-  updateActiveDot();
+function moveWheel(amount, i, index) {
+
+    let progress = tl.progress();
+    tl.progress(wrapProgress(snap(tl.progress() + amount)))
+    let next = tracker.item;
+    tl.progress(progress);
+
+    document.querySelector('.item.active').classList.remove('active');
+    items[next].classList.add('active');
+
+    gsap.to(tl, {
+        progress: snap(tl.progress() + amount),
+        modifiers: {
+            progress: wrapProgress
+        }
+    });
 }
-
-
-createDots();
-showSlide(currentIndex);
-updateActiveDot();
-setTimeout(autoSlide, 5000); // Display each slide for 10 seconds
-setInterval(autoSlide, 5000); // Auto slide every 8 seconds
--- sterling-reviews.php
+++ sterling-reviews.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * @version 1.0.0 @package sterling-reviews
+ * @version 2.0.0 @package sterling-reviews
  * Plugin Name: Google Reviews by Heigh10
+ * Version: 2.0
  * Author: Heigh10.com
  * Author URI: https://heigh10.com
  * Description: Google reviews Showcase, using json file & cobra_testimonials post_type. Use shortcode [google_reviews_sterling] to display anywhere
  */
 
  add_action( 'wp_enqueue_scripts', function () {
-  wp_enqueue_style( 'testimonials', plugins_url( '/front-end/style.css' , __FILE__ ), false, '1.0', 'all');
-  wp_enqueue_style( 'font-awesome-6.4', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',false,'6.4','all');
-  if(is_front_page()) {
-  wp_enqueue_script('testimonials',plugins_url( '/front-end/script.js', __FILE__ ), false,'1.0', true );}
+    wp_enqueue_style( 'testimonials', plugins_url( '/front-end/style.css' , __FILE__ ), false, '1.0', 'all');
+    wp_enqueue_script('gsap',"https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js", false,'1.0',true);
+    wp_enqueue_script('motionpath-plugin-gsap',"https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/MotionPathPlugin.min.js", false,'1.0',true);
+    wp_enqueue_script('testimonials',plugins_url( '/front-end/script.js', __FILE__ ), array('jquery'),'1.0',true);
+    wp_enqueue_style( 'font-awesome-6.4', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',false,'6.4','all');
 } );
 
 add_shortcode( 'google_reviews_sterling', function()
@@ -19,59 +21,55 @@ add_shortcode( 'google_reviews_sterling', function()
   $str = file_get_contents(plugins_url( '/reviews.json', __FILE__ ));
   $json = json_decode($str, true); 
   $data = $json['reviews'];
+  $arr = [];
+  $slides = "";
 
-  $image = ""; $slides = ""; $count = 0;
-
-  for ($i=0; $count < 10 ; $i++) { 
-      $random = (int)rand(0, sizeof($data));
-      
-      if($data[$random]['starRating']!=="FIVE" || !array_key_exists("comment",$data[$random])) continue;
-  
-  $slides .= '<div class="slide">
-  <p class="description elementor-heading-title">
-    '.$data[$random]["comment"].'
-  </p>
-  <div class="name">
-  '.$data[$random]["reviewer"]["displayName"].'
-  </div>
-  <div class="ratings">
-  <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
-  </div>
-</div>'; 
-$count ++;
+  for ($i=0; sizeof($arr) < 10 ; $i++) { 
+      $extra_class = sizeof($arr)===0?"active":"";
+      $random = rand(0,sizeof($data)-1);
+    if($data[$random]['starRating']!=="FIVE" || !array_key_exists("comment",$data[$random]) || in_array($random,$arr)) continue;
+    array_push($arr,$random);
+  $slides .= '
+  <div class="testimonial '.$extra_class.'">
+                    <div class="review">
+                       '.$data[$random]["comment"].'
+                    </div>
+                    <div class="name">
+                        '.$data[$random]["reviewer"]["displayName"].' | Google
+                    </div>
+                    <div class="ratings">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+                </div>'; 
 }
     return '
-    <div class="testimonials">
-    <div class="row">
-      <div class="col-40">
-        <div class="person-slider">
-          <div class="person-slides">
-          <div class="person-slide slide-0">
-    <img src="https://sterlingwpcobra.s3.us-west-1.amazonaws.com/wp-content/uploads/2023/05/09091439/quotes-face-5.png" alt="Sterling Google Reviews">
-  </div>
-          <div class="person-slide slide-1">
-    <img src="https://sterlingwpcobra.s3.us-west-1.amazonaws.com/wp-content/uploads/2023/05/09091441/quotes-face-4.png" alt="Sterling Google Reviews">
-  </div>
-          <div class="person-slide slide-2">
-    <img src="https://sterlingwpcobra.s3.us-west-1.amazonaws.com/wp-content/uploads/2023/05/09091441/quotes-face-3.png" alt="Sterling Google Reviews">
-  </div>
-          <div class="person-slide slide-3">
-    <img src="https://sterlingwpcobra.s3.us-west-1.amazonaws.com/wp-content/uploads/2023/05/09091441/quotes-face-1.png" alt="Sterling Google Reviews">
-  </div>
-          <div class="person-slide slide-4">
-    <img src="https://sterlingwpcobra.s3.us-west-1.amazonaws.com/wp-content/uploads/2023/05/09091443/quotes-face-2.png" alt="Sterling Google Reviews">
-  </div>
-          </div>
+    <div class="g-reviews" id="g-reviews">
+    <div class="google-row">
+        <div class="google-col-1">
+            <div class="left">
+            <div class="nav-container" style="text-align: center;">
+                <button id="prev" class="nav">&#8592;</button>
+                <button id="next" class="nav">&#8594;</button>
+                <span class="number" id="number"></span>
+              </div>
+            <div class="wrapper">
+                <div class="dots"></div>
+                <svg viewBox="0 0 3200 3200" id="viewBox">
+                  <circle id="holder" class="circle" cx="1600" cy="1600" r="800"/>
+                </svg>
+              </div>
+            </div>
         </div>
-      </div>
-      <div class="col-60">
-        <div class="slider">
-          <div class="slides">
-            '.$slides.'                     
+        <div class="google-col-2">
+            <div class="content-wrapper" id="content">                
+                '.$slides.'
+            </div>
         </div>
-        <div class="dots"></div>
-      </div>
     </div>
-    </div>
+</div>
       ';
 });
